6932bea932355223da931ff2e61463445a81596c
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-01-05 15:26:17 +0200

    done with the form validation.

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -4,10 +4,23 @@ import { HeroForm } from './hero-form';
 @Component({
     moduleId: module.id,
     selector: 'hero-form',
-    templateUrl: 'hero-form.component.html'
+    templateUrl: 'hero-form.component.html',
+    styleUrls: ['hero-form.component.css']
 })
 export class HeroFormComponent {
  
     powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
+    model = new HeroForm(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+    
+    submitted = false;
+    onSubmit() { this.submitted = true; }
+    
+    newHero() {
+        this.model = new HeroForm(42,'','', '');
+    }
+
+    // TODO: Remove this when we're done
+    get diagnostic() { return JSON.stringify(this.model); }
 
 }
